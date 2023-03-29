da88e2e429ba4d130b793b0e8d680e71eaa87c11
Geraldine <galongo@uci.edu>
2023-03-28 20:14:26 -0700

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,8 @@
 const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
 
 //1. use d3 to load dataset
-d3.json(url).then(function(data) {
+//call init function to initialize the dropdown menu and the charts
+d3.json(url).then(init, function(data) {
     console.log(data);
 }, function(error) {
     console.log(error);
@@ -20,6 +21,8 @@ function init(data) {
         console.log(id);
         dropdownMenu.append('option').text(id).property('value', id);
     });
+    //event listener to dropdown menu
+    dropdownMenu.on('change', optionchanged);
     //variable containing value of first sample
     let firstsample = samples[0];
     //log value of first sample
@@ -31,41 +34,38 @@ function init(data) {
     buildGaugeChart(firstsample, data);
 }
 
-//call init function to initialize the dropdown menu and the charts
-d3.json(url).then(init, function(error) {
-    console.log(error);
-});
-
 //2. create horizontal barchart with dropdown displaying top 10 OTUs in individual
-function buildBarChart(sample, data) {
+function buildBarChart(id, data) {
     //get data for selected sample
-    let individualData = data.samples.find(element => element.id == sample);
+    let individualData = data.samples.filter(sample => sample.id === id)[0];
     //sort data by OTU count in descending order and get the top 10 OTUs
     let top10OTUs = individualData.otu_ids.slice(0, 10).reverse();
     let top10Counts = individualData.sample_values.slice(0, 10).reverse();
     let top10Labels = individualData.otu_labels.slice(0, 10).reverse();
-
+  
     //create bar chart
     let bar_data = [{
-        //use sample_values as values for barchart
-        values: top10Counts,
-        //use otu_ids as labels for barchart
-        labels: top10OTUs,
-        //set type of chart to bar
-        type: "bar",
-        //use otu_labels as hovertext for chart
-        hovertext: top10Labels,
-        //set chart orientation
-        orientation: "h"
+      //use sample_values as values for barchart
+      x: top10Counts,
+      //use otu_ids as labels for barchart
+      y: top10OTUs.map(otu => `OTU ${otu}`),
+      //set type of chart to bar
+      type: "bar",
+      //use otu_labels as hovertext for chart
+      text: top10Labels,
+      //set chart orientation
+      orientation: "h"
     }];
-
+  
     let layout = {
-        height: 400,
-        width: 800
+      title: "Top 10 OTUs",
+      height: 400,
+      width: 800
     };
-
+  
     Plotly.newPlot("bar", bar_data, layout);
-};
+  };
+  
 
 //3. create bubble chart displaying each sample
 function buildBubbleChart(sample, data) {
@@ -80,7 +80,7 @@ function buildBubbleChart(sample, data) {
         //set type of chart to bar
         mode: "markers",
         //modify the markers
-        markers: {
+        marker: {
             //use otu_ids for marker colors
             color: individualData.otu_ids,
             //use sample_values for marker size
@@ -89,12 +89,18 @@ function buildBubbleChart(sample, data) {
         //use otu_labels for text values
         text: individualData.otu_labels
     }];
-
+    //chart layout
     let layout = {
+        title: "Bacteria in Sample",
         height: 500,
-        width: 1000
+        width: 1000,
+        xaxis: {
+            title: "OTU ID"
+        },
+        yaxis: {
+            title: "Sample Value"
+        }
     };
-
     Plotly.newPlot("bubble", bubble_data, layout);
 };
 
@@ -133,57 +139,27 @@ function optionchanged() {
         buildBubbleChart(selectedSample, data);
         // update the metadata
         buildMetadata(selectedSample, data);
+        // update the gauge chart
+        buildGaugeChart(selectedSample, data);
     });
 };
 
 //BONUS: adapt Gauge Chart from  https://plot.ly/javascript/gauge-charts/ to plot weekly washing frequency of individual
 function buildGaugeChart(sample) {
     // Filter the data for the selected sample
-    var metadata = data.metadata.filter(obj => obj.id == sample)[0];
-    var washFreq = metadata.wfreq;
-
+    let metadata = data.metadata.filter(obj => obj.id == sample)[0];
+    let washFreq = metadata.wfreq;
     // Create the gauge chart
-    var data = [
+    let data = [
         {
             domain: { x: [0, 1], y: [0, 1] },
             value: washFreq,
             title: { text: "Belly Button Washing Frequency<br><sub>Scrubs per Week</sub>" },
             type: "indicator",
-            mode: "gauge+number",
-            gauge: {
-            //modify gauge code for values randing from 0 through 9
-            axis: { range: [0, 9], tickwidth: 1, tickcolor: "black" },
-            bar: { color: "darkblue" },
-            bgcolor: "white",
-            borderwidth: 2,
-            bordercolor: "gray",
-            steps: [
-                { range: [0, 1], color: "#f7e8e8" },
-                { range: [1, 2], color: "#f2d9d9" },
-                { range: [2, 3], color: "#e6b8b8" },
-                { range: [3, 4], color: "#e0aaaa" },
-                { range: [4, 5], color: "#d37d7d" },
-                { range: [5, 6], color: "#c77a7a" },
-                { range: [6, 7], color: "#b35050" },
-                { range: [7, 8], color: "#a64444" },
-                { range: [8, 9], color: "#8f2e2e" }
-            ]}}];
+            mode: "gauge+number"
+        }];
     // Set the layout for the gauge chart
-    var layout = { width: 400, height: 300, margin: { t: 0, b: 0 } };
+    let layout = {title: "Wash Frequency", width: 400, height: 300, margin: { t: 0, b: 0 } };
     // Use Plotly to plot the gauge chart on the "gauge-chart" div
     Plotly.newPlot("gauge", data, layout);
 };
-  
-//update with each sample that is selected
-function updateGaugeChart(value) {
-    var gaugeChart = document.getElementById("gauge-chart");
-    Plotly.animate(gaugeChart, {
-        data: [{ value: value }]
-    }, {
-        transition: {
-            duration: 500,
-            easing: "cubic-in-out"
-        }
-    });
-};
-
